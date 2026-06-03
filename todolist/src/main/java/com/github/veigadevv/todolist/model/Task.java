8305440e38284cf14bd8771d9397dbe28a2b7a6a
VeigaDevv <veigadevv@gmail.com>
2026-06-03 13:52:22 -0700

    feat: configuracao inicial do projeto e entidade Task

---
+++ todolist/src/main/java/com/github/veigadevv/todolist/model/Task.java
@@ -0,0 +1,43 @@
+package com.github.veigadevv.todolist.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O título é obrigatório")
+    @Size(min = 3, max = 100, message = "O título deve ter entre 3 e 100 caracteres")
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private boolean completed = false;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    // Código que roda automaticamente antes de salvar uma nova tarefa no banco
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
