9f91cf72aa057c2e1933e19524a3bedc67ba8aff
VeigaDevv <veigadevv@gmail.com>
2026-06-10 14:32:12 -0700

    feat: implementado tratamento global de erros com ControllerAdvice para recurso nao encontrado

---
+++ todolist/src/main/java/com/github/veigadevv/todolist/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.github.veigadevv.todolist.exceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        String error = "Recurso não encontrado";
+        HttpStatus status = HttpStatus.NOT_FOUND; // Erro 404
+
+        StandardError err = new StandardError(
+                Instant.now(),
+                status.value(),
+                error,
+                e.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(status).body(err);
+    }
+}
